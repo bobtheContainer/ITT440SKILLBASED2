8b663c8f961ca5b7e809381fd9ec9b38b21c4194
bobtheContainer <99312454+bobtheContainer@users.noreply.github.com>
2023-01-26 14:05:10 +0800

    Update Q3client.py

--- Q3client.py
+++ Q3client.py
@@ -2,7 +2,7 @@ import socket
 #Q3 CLIENT
 # c = client
 c = socket.socket()
-c.connect(("192.168.188.128", 4444))
+c.connect(("192.168.188.128", 8888))
 
 quote_reply = c.recv(4096).decode()
 print(quote_reply)
